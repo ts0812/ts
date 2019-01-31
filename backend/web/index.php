0860dba630c299c0879026b1ace91801324c1a78
ts <1352645017@qq.com>
2019-01-31 22:33:15 +0800

    配置修改

--- backend/web/index.php
+++ backend/web/index.php
@@ -7,9 +7,9 @@ require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
 require __DIR__ . '/../config/bootstrap.php';
 if(isset($_SERVER['ENV_SERVER']) && $_SERVER['ENV_SERVER'] == 'aliyun'){
-    require __DIR__ . '/../config/define.php';
+    require __DIR__ . '/../../common/config/define.php';
 }else{
-    require __DIR__ . '/../config/define-local.php';
+    require __DIR__ . '/../../common/config/define-local.php';
 }
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
